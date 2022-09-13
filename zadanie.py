c29baf8a7fef8a6d6e0d699b828622272eb61dc8
DiKey10 <kd930603@gmail.com>
2022-09-13 17:29:17 +0200

    waat

--- zadanie.py
+++ zadanie.py
@@ -1,5 +1,32 @@
-import requests
+import requests,csv
 
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
 data = response.json()[0]
-print(data["rates"])
+################
+
+#print(data["rates"])
+dane_csv = data["rates"]
+
+################
+
+zerowe=dane_csv[0]
+zmienna_slowa = ""
+for item in zerowe:
+    zmienna_slowa = zmienna_slowa + item + ";"
+
+aktualne_dane_csv=zmienna_slowa[:-1]
+
+####################
+
+raz=[]
+
+for x in range(0,8):
+    zmienna_slowa2 = ""
+    for item in dane_csv[x].values():
+        zmienna_slowa2 = zmienna_slowa2 + str(item) + ";"
+    aktualne_dane_csv2 = zmienna_slowa2[:-1]
+    raz.append(aktualne_dane_csv2)
+print(raz)
+
+
+
